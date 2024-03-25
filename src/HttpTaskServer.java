6f4366159219ae4f1945be6995385d1ecdf1893a
Elena Polukova <e.brykova@yofin.ru>
2024-03-25 16:22:10 +0400

    refactoring: HttpTaskServer

--- src/HttpTaskServer.java
+++ src/HttpTaskServer.java
@@ -7,9 +7,11 @@ import java.net.InetSocketAddress;
 public class HttpTaskServer {
     private static final int PORT = 8080;
 
-    public static void main(String[] args) {
+    private static HttpServer httpServer;
+
+    public static void start() {
         try {
-            HttpServer httpServer = HttpServer.create();
+            httpServer = HttpServer.create();
             httpServer.bind(new InetSocketAddress(PORT), 0);
 
             httpServer.createContext("/tasks", new TasksHandler());
@@ -24,6 +26,10 @@ public class HttpTaskServer {
         } catch (IOException exception) {
             System.out.println("Ошибка запуска HTTP-сервера: " + exception);
         }
+    }
 
+    public static void stop() {
+        httpServer.stop(0);
+        System.out.println("HTTP-сервер запущен остановлен");
     }
 }
